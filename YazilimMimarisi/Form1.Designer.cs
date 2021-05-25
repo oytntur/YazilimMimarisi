9fc3f8b03f4de3b2ac2f08b1cf703e9901f19d7e
oytntur <66783409+oytntur@users.noreply.github.com>
2021-05-25 17:48:55 +0300

    Proje Temelleri Atıldı

--- YazilimMimarisi/Form1.Designer.cs
+++ YazilimMimarisi/Form1.Designer.cs
@@ -29,20 +29,133 @@ namespace YazilimMimarisi
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.btnLogin = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.txtBoxPass = new Bunifu.Framework.UI.BunifuTextbox();
+            this.txtBoxUsername = new Bunifu.Framework.UI.BunifuTextbox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // bunifuImageButton1
+            // 
+            this.bunifuImageButton1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuImageButton1.Enabled = false;
+            this.bunifuImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton1.Image")));
+            this.bunifuImageButton1.ImageActive = null;
+            this.bunifuImageButton1.Location = new System.Drawing.Point(252, 48);
+            this.bunifuImageButton1.Name = "bunifuImageButton1";
+            this.bunifuImageButton1.Size = new System.Drawing.Size(214, 142);
+            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton1.TabIndex = 18;
+            this.bunifuImageButton1.TabStop = false;
+            this.bunifuImageButton1.Zoom = 10;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.ActiveBorderThickness = 1;
+            this.btnLogin.ActiveCornerRadius = 20;
+            this.btnLogin.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.btnLogin.ActiveForecolor = System.Drawing.Color.White;
+            this.btnLogin.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.btnLogin.BackColor = System.Drawing.SystemColors.Control;
+            this.btnLogin.ButtonText = "Sign In";
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.SeaGreen;
+            this.btnLogin.IdleBorderThickness = 1;
+            this.btnLogin.IdleCornerRadius = 20;
+            this.btnLogin.IdleFillColor = System.Drawing.Color.White;
+            this.btnLogin.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.btnLogin.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.btnLogin.Location = new System.Drawing.Point(265, 405);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(5);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(181, 41);
+            this.btnLogin.TabIndex = 16;
+            this.btnLogin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtBoxPass
+            // 
+            this.txtBoxPass.BackColor = System.Drawing.Color.Silver;
+            this.txtBoxPass.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("txtBoxPass.BackgroundImage")));
+            this.txtBoxPass.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.txtBoxPass.ForeColor = System.Drawing.Color.SeaGreen;
+            this.txtBoxPass.Icon = ((System.Drawing.Image)(resources.GetObject("txtBoxPass.Icon")));
+            this.txtBoxPass.Location = new System.Drawing.Point(284, 297);
+            this.txtBoxPass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtBoxPass.Name = "txtBoxPass";
+            this.txtBoxPass.Size = new System.Drawing.Size(253, 52);
+            this.txtBoxPass.TabIndex = 13;
+            this.txtBoxPass.text = "";
+            // 
+            // txtBoxUsername
+            // 
+            this.txtBoxUsername.BackColor = System.Drawing.Color.Silver;
+            this.txtBoxUsername.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("txtBoxUsername.BackgroundImage")));
+            this.txtBoxUsername.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.txtBoxUsername.ForeColor = System.Drawing.Color.SeaGreen;
+            this.txtBoxUsername.Icon = ((System.Drawing.Image)(resources.GetObject("txtBoxUsername.Icon")));
+            this.txtBoxUsername.Location = new System.Drawing.Point(284, 237);
+            this.txtBoxUsername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtBoxUsername.Name = "txtBoxUsername";
+            this.txtBoxUsername.Size = new System.Drawing.Size(253, 52);
+            this.txtBoxUsername.TabIndex = 12;
+            this.txtBoxUsername.text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(165, 310);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Şifre :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(131, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 25);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Kullanıcı Adı :";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(815, 667);
+            this.ClientSize = new System.Drawing.Size(853, 624);
+            this.Controls.Add(this.bunifuImageButton1);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtBoxPass);
+            this.Controls.Add(this.txtBoxUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton1;
+        private Bunifu.Framework.UI.BunifuThinButton2 btnLogin;
+        private Bunifu.Framework.UI.BunifuTextbox txtBoxPass;
+        private Bunifu.Framework.UI.BunifuTextbox txtBoxUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- YazilimMimarisi/Form1.cs
+++ YazilimMimarisi/Form1.cs
@@ -7,14 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace YazilimMimarisi
 {
+
     public partial class Form1 : Form
     {
+        SqlCommand cmd;
+        SqlDataReader dr;
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-S1IT89F\\SQLEXPRESS;Initial Catalog=DB_Diyet;Integrated Security=True");
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand("SELECT * from tblUser where userLogin='" + txtBoxUsername.text + "'AND userPassword='" + txtBoxPass.text + "'", con);
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                
+                if (Convert.ToInt32(dr["yetkiID"]) == 2 && Convert.ToBoolean(dr["isVerified"]) == true)
+                {
+                    MessageBox.Show(dr["userAd"].ToString() + " " + dr["userSoyad"].ToString()
+                    + "\nHoşgeldiniz", "Giriş Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    //kullaniciEkran kullaniciEkran = new kullaniciEkran(Convert.ToInt32(dr["userID"]));
+                    //kullaniciEkran.Show();
+                    //this.Hide();
+                }
+                else if (Convert.ToInt32(dr["yetkiID"]) == 1)
+                {
+                    adminEkran adminEkran = new adminEkran();
+                    adminEkran.Show();
+                    this.Hide();
+                }
+                else if (Convert.ToBoolean(dr["isVerified"]) == false)
+                {
+                    MessageBox.Show("Sayın " + dr["userAd"].ToString() + " " + dr["userSoyad"].ToString()
+                    + "\nHoşgeldiniz"+ "\nGiriş Başarısız Lütfen Yöneticinin" +
+                    " Onayını Bekleyiniz","Giriş Başarısız", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+               
+            }
+            else
+            {
+                MessageBox.Show("Kullanici Adı Veya Şifre Yanlış", "Hata",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            con.Close();
         }
     }
 }
