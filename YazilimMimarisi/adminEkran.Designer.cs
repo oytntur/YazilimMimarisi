f7916f8e86841969f67abd1ed7a460f8c1d61a15
oytntur <66783409+oytntur@users.noreply.github.com>
2021-05-25 18:02:16 +0300

    Diyetisyen onayı hazırlandı çalışır hale getirildi

--- YazilimMimarisi/adminEkran.Designer.cs
+++ YazilimMimarisi/adminEkran.Designer.cs
@@ -29,13 +29,40 @@ namespace YazilimMimarisi
         /// </summary>
         private void InitializeComponent()
         {
+            this.transListView = new System.Windows.Forms.ListView();
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.urunAd = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
+            // 
+            // transListView
+            // 
+            this.transListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID,
+            this.urunAd});
+            this.transListView.HideSelection = false;
+            this.transListView.Location = new System.Drawing.Point(12, 12);
+            this.transListView.Name = "transListView";
+            this.transListView.Size = new System.Drawing.Size(625, 476);
+            this.transListView.TabIndex = 2;
+            this.transListView.UseCompatibleStateImageBehavior = false;
+            this.transListView.View = System.Windows.Forms.View.Details;
+            this.transListView.DoubleClick += new System.EventHandler(this.transListView_DoubleClick);
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            // 
+            // urunAd
+            // 
+            this.urunAd.Text = "Diyetisyen Adı";
+            this.urunAd.Width = 130;
             // 
             // adminEkran
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(891, 655);
+            this.Controls.Add(this.transListView);
             this.Name = "adminEkran";
             this.Text = "adminEkran";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.adminEkran_FormClosed);
@@ -44,5 +71,9 @@ namespace YazilimMimarisi
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView transListView;
+        private System.Windows.Forms.ColumnHeader ID;
+        private System.Windows.Forms.ColumnHeader urunAd;
     }
 }
--- YazilimMimarisi/adminEkran.cs
+++ YazilimMimarisi/adminEkran.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,14 +13,63 @@ namespace YazilimMimarisi
 {
     public partial class adminEkran : Form
     {
+        SqlCommand cmd;
+        SqlDataReader dr;
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-S1IT89F\\SQLEXPRESS;Initial Catalog=DB_Diyet;Integrated Security=True");
+        int id = 0;
         public adminEkran()
         {
             InitializeComponent();
+            getDoctor();
+        }
+        private void getDoctor()
+        {
+            transListView.Items.Clear();
+            cmd = new SqlCommand("SELECT * FROM unVerifiedDoctorView", con);
+            con.Open();
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                ListViewItem add = new ListViewItem();
+                add.Text = dr["ID"].ToString();
+                add.SubItems.Add(dr["Ad"].ToString() + " " + dr["Soyad"].ToString());
+                transListView.Items.Add(add);
+
+            }
+            dr.Close();
+            con.Close();
         }
 
         private void adminEkran_FormClosed(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void transListView_DoubleClick(object sender, EventArgs e)
+        {
+            id = int.Parse(transListView.SelectedItems[0].SubItems[0].Text);
+            con.Open();
+            cmd = new SqlCommand("EXEC docConfirm '" + id + "'", con);
+            DialogResult dialogResult = MessageBox.Show("Sure", "Some Title", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    getDoctor();
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do something else
+            }
         }
     }
 }
